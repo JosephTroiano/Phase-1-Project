5423150889e3013651705ec3b0fd5e5239e5c369
Joseph <joseph.m.troiano@gmail.com>
2023-07-04 00:15:32 -0400

    Added form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', fetchRecipes)
 
 function fetchRecipes() {
-    const url = http://localhost:3000/recipes
+    const url = `http://localhost:3000/recipes`
     fetch(url)
     .then(response => response.json())
     .then(recipes => {
@@ -15,9 +15,9 @@ function fetchRecipes() {
 }
 
 function createRecipeCard(recipe) {
-    const recipeShowcase = document.getElementById('recipes-showcase');
+    const recipeShowcase = document.getElementById('recipe-container');
 
-    const recipeCard = document.createElement('li');
+    const recipeCard = document.createElement('div');
     recipeCard.classList.add('recipe-card');
 
     const recipeName = document.createElement('h2')
@@ -26,7 +26,20 @@ function createRecipeCard(recipe) {
     const recipeImage = document.createElement('img')
     recipeImage.src = recipe.image;
 
+    const showRecipe = document.createElement('button')
+    showRecipe.id = 'details-btn'
+    showRecipe.textContent = 'View Recipe'
+
     const recipeLikes = document.createElement('button')
     recipeLikes.textContent = `Likes: ${recipe.likes}`
+    recipeLikes.id = 'likes-btn'
+
+
+    recipeCard.appendChild(recipeName)
+    recipeCard.appendChild(recipeImage)
+    recipeCard.appendChild(showRecipe)
+    recipeCard.appendChild(recipeLikes)
+
+    recipeShowcase.appendChild(recipeCard)
 
 }
